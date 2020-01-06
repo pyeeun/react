21bf6de4c1e3e307a59d858395cecb82ca58b1d1
yeeun park <charm5231@naver.com>
2020-01-06 23:12:31 +0900

    이벤트 핸들링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Say from './Say';
-import './App.css';
+import EventPractice from './EventPractice';
 
 const App = () => {
-  return <Say/>
+  return <EventPractice/>;
 };
 export default App;
